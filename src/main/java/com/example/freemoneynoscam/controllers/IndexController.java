a9dae02405454a2af58d9596090400c3fbdfb13d
OliverBark <oliver.bark@icloud.com>
2022-03-23 11:15:23 +0100

    c

--- src/main/java/com/example/freemoneynoscam/controllers/IndexController.java
+++ src/main/java/com/example/freemoneynoscam/controllers/IndexController.java
@@ -1,26 +1,44 @@
 package com.example.freemoneynoscam.controllers;
 
+import com.example.freemoneynoscam.services.EmailsToSQL;
+import com.example.freemoneynoscam.services.ValidateEmailService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.WebRequest;
 
 @Controller
 public class IndexController {
+
     @GetMapping("/")
-    public String index(){
+    public String index() {
         return "index";
     }
+    @PostMapping("/get-free-money-success")
+    public String getFreeMoney(WebRequest dataFromForm) {
 
-    @PostMapping("/test")
-    public String test(WebRequest dataFromForm){
-        System.out.println(dataFromForm.getParameter("email"));
-        System.out.println(dataFromForm.getParameter("email"));
-        System.out.println(dataFromForm.getParameter("email"));
-        System.out.println(dataFromForm.getParameter("email"));
-        System.out.println(dataFromForm.getParameter("email"));
+        String email = dataFromForm.getParameter("email");
+        ValidateEmailService validateEmailService = new ValidateEmailService();
+        EmailsToSQL emailsToSQL = new EmailsToSQL();
+
+        if (validateEmailService.isEmailValid(email)) {
+            System.out.println(dataFromForm.getParameter("email"));
+            emailsToSQL.insertEmailToDB(email);
+            return "get-free-money-success";
+        } else {
+            System.out.println("Invalid email");
+        }
         return "redirect:/";
+
     }
+    @GetMapping("/get-free-money-success")
+    public String getMoney() {
+        return "get-free-money-success";
+    }
+
 }
+
+
+
+
+
--- src/main/java/com/example/freemoneynoscam/services/ValidateEmailService.java
+++ src/main/java/com/example/freemoneynoscam/services/ValidateEmailService.java
@@ -1,8 +1,15 @@
 package com.example.freemoneynoscam.services;
 
 public class ValidateEmailService {
-    public boolean isEmailValid(String email){
-        //TODO implement logic such that we verify an e-mail is valid
-        return true;
+
+
+    public boolean isEmailValid(String email) {
+
+        if (email.contains("@") && email.contains(".") && email.length() > 5){
+            return true;
+        }
+
+        return false;
     }
 }
+
