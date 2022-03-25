060db06192e58955a9da5a64874f4b9cd03983bb
sofiaabrahamsen <sofia.abrahamsen@yahoo.com>
2022-03-25 10:22:24 +0100

    commit

--- src/main/java/com/example/freemoneynoscam/services/Database.java
+++ src/main/java/com/example/freemoneynoscam/services/Database.java
@@ -13,7 +13,7 @@ public class Database {
         try
         {
             String url = "jdbc:mysql://localhost:3306/free_money";
-            con = DriverManager.getConnection(url,"root","testtest");
+            con = DriverManager.getConnection(url,"root","Sofia1864");
             System.out.println("Ok, we have a connection.");
         }
         catch(Exception e)
